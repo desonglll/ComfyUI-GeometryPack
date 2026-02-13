2f567210c9998d7baed79a311910a2e42ef4f363
PozzettiAndrea <andrea@pozzetti.it>
2026-02-13 01:18:40 +0100

    dynamic widgets ver bump

--- __init__.py
+++ __init__.py
@@ -6,8 +6,8 @@ print("[geompack] calling register_nodes", file=sys.stderr, flush=True)
 NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS = register_nodes()
 
 
-from comfy_dynamic_widgets import write_mappings
-write_mappings(NODE_CLASS_MAPPINGS, __file__)
+from comfy_dynamic_widgets import generate_mappings
+generate_mappings(NODE_CLASS_MAPPINGS)
 
 WEB_DIRECTORY = "./web"
 __all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS', 'WEB_DIRECTORY']
